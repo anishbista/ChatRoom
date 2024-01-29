a394bfedc9b13a7f9650188bfbdcc444cb5abd86
anishbista92 <anishbista9235@gmail.com>
2024-01-29 18:09:40 +0545

    Chat Room finished

--- chat/consumers.py
+++ chat/consumers.py
@@ -9,7 +9,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.roomGroupName, self.channel_layer)
+        await self.channel_layer.group_discard(self.roomGroupName, self.channel_name)
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
